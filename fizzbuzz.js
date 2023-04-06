f424ced9a02af868d7b827a0958953807c4d38f5
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-06 14:46:01 -0500

    Custom Solution for Coding Challenge

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,3 +1,17 @@
-module.exports = function () {
-  // write your code here
+module.exports = function (str1 = 'Fizz', str2 = 'Buzz', n1 = 3, n2 =5) {
+  let arr = [];
+
+  for(let i = 1; i <= 100; i++) {
+    if(i % n1 === 0 && i % n2 === 0) {
+      arr.push(str1+str2)
+    } else if (i % n1 === 0) {
+      arr.push(str1)
+    }
+    else if (i % n2 === 0) {
+      arr.push(str2);
+    } else {
+      arr.push(i)
+    }  
+}
+return arr
 }
